c6f4eeb1156e56a6fd62fc7218ed31850eb6aaed
Great4072 <s68122250081@ssru.ac.th>
2026-01-12 00:43:02 +0700

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    String studentId;
+    String name;
+    String major;
+    double gpa;
+
+    // constructor
+    public Student(String studentId, String name, String major, double gpa) {
+        this.studentId = studentId;
+        this.name = name;
+        this.major = major;
+        this.gpa = gpa;
+    }
+
+    // method แสดงข้อมูล
+    public void displayInfo() {
+        System.out.println("รหัสนักศึกษา: " + studentId);
+        System.out.println("ชื่อ: " + name);
+        System.out.println("สาขา: " + major);
+        System.out.println("เกรดเฉลี่ย: " + gpa);
+        System.out.println("----------------------");
+    }
+}
